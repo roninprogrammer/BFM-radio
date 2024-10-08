7b8488a008f4f15cd9407113bf2f1911d67b9558
v1ckn35hB <vickneshb@maxis.com.my>
2024-10-08 14:11:29 +0800

    change all static to dynamic value

--- app/src/main/java/com/bfmradio/app/MainActivity.kt
+++ app/src/main/java/com/bfmradio/app/MainActivity.kt
@@ -1,34 +1,60 @@
 package com.bfmradio.app
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import com.bfmradio.app.ui.screens.PodcastScreen
 import com.bfmradio.app.ui.theme.BFMTheme
 import com.bfmradio.app.ui.viewmodels.PodcastViewModel
+import com.bfmradio.app.utils.ConnectivityObserver
+import com.bfmradio.app.utils.Utils
+
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            // This is entry point of BFM application where main Compose UI being setup
-            val podcastViewModel: PodcastViewModel by viewModels() // Initialize ViewModel properly
+        val podcastViewModel: PodcastViewModel by viewModels()
+        val connectivityObserver = ConnectivityObserver(applicationContext)
 
+        setContent {
             BFMTheme {
+                var isOnline by remember { mutableStateOf(true) }
+                var snackbarVisible by remember { mutableStateOf(false) }
+
+                LaunchedEffect(Unit) {
+                    connectivityObserver.observeNetworkStatus().collect { status ->
+                        isOnline = status
+                        snackbarVisible = !isOnline
+                    }
+                }
+
+
                 PodcastScreen(viewModel = podcastViewModel)
+                if (snackbarVisible) {
+                    Snackbar(
+                        action = {
+                            Button(onClick = {
+                                val intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                                startActivity(intent)
+                            }) {
+                                Text(Utils.settings())
+                            }
+                        }
+                    ) {
+                        Text(Utils.lostConnection())
+                    }
+                }
             }
         }
     }
 }
+
 
--- app/src/main/java/com/bfmradio/app/utils/ConnectivityObserver.kt
+++ app/src/main/java/com/bfmradio/app/utils/ConnectivityObserver.kt
@@ -33,4 +33,39 @@ class ConnectivityObserver(context: Context) {
             connectivityManager.unregisterNetworkCallback(networkCallback)
         }
     }
+
+    private fun determineNetworkStatus(capabilities: NetworkCapabilities?): NetworkStatus {
+        return when {
+            capabilities == null -> NetworkStatus.Unknown
+            capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                    capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) -> {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    if (capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)) {
+                        NetworkStatus.GoodWifi
+                    } else {
+                        NetworkStatus.PoorWifi
+                    }
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    if (capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)) {
+                        NetworkStatus.GoodMobile
+                    } else {
+                        NetworkStatus.PoorMobile
+                    }
+                } else {
+                    NetworkStatus.Unknown
+                }
+            }
+            else -> NetworkStatus.Disconnected
+        }
+    }
+
+    sealed class NetworkStatus {
+        object GoodWifi : NetworkStatus()
+        object PoorWifi : NetworkStatus()
+        object GoodMobile : NetworkStatus()
+        object PoorMobile : NetworkStatus()
+        object Disconnected : NetworkStatus()
+        object Unknown : NetworkStatus()
+    }
+
 }
